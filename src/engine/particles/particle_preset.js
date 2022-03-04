370820fe6665547cc530965f405c3e17f27b34e0
nmart510 <nicholas.pa.ma@gmail.com>
2022-03-03 16:10:57 -0800

    adjusted flame particle preset

--- src/engine/particles/particle_preset.js
+++ src/engine/particles/particle_preset.js
@@ -38,19 +38,17 @@ function _createFlame(atX, atY) {
     let p = new FlameParticle(engine.defaultResources.getDefaultPSTexture(), atX, atY, life);
     p.setColor([1, 0, 0, 1]);
     // size of the particle
-    let r = 5.5 + Math.random() * 0.5;
+    let r = 2.5 + Math.random() * 0.5;
     p.setSize(r, r);
     // final color
-    let fr = 3.5 + Math.random();
-    let fg = 0.4 + 0.1 * Math.random();
-    let fb = 0.3 + 0.1 * Math.random();
-    p.setFinalColor([fr, fg, fb, 0.6]);
+    p.setFinalColor([1, .8, .3, 0.6]);
     
     // velocity on the particle
     let fx = 10 - 20 * Math.random();
     let fy = 10 * Math.random();
     p.setVelocity(fx, fy);
     p.setAcceleration(0,20);
+    p.setSpread(.5);
     // size delta
     p.setSizeDelta(0.98);
     
