605648521164f6e380e64f174f9d167e5d1943fc
a-calkins <81609703+a-calkins@users.noreply.github.com>
2022-03-10 18:08:03 -0800

    Update color.js

--- src/engine/color.js
+++ src/engine/color.js
@@ -2,6 +2,12 @@
 "use strict";
 
 class Color {
-
+    static RED = vec4.fromValues(1, 0, 0, 1); //red
+    static YELLOW = vec4.fromValues(1, 0.92, 0.016, 1); // it's fucking yellow!
+    static GREEN = vec4.fromValues(0, 1, 0, 1); // it's goddamn green
+    static BLUE = vec4.fromValues(0, 0, 1, 1);
+    static MAGENTA = vec4.fromValues(1, 0, 1, 1);
+    static BLACK = vec4.fromValues(0, 0, 0, 1);
+    static WHITE = vec4.fromValues(1, 1, 1, 1);
 }
 export default Color;
