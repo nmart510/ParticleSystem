778fd82c27caa3d01b297fb3792bcf4d48c99c97
nmart510 <nicholas.pa.ma@gmail.com>
2022-03-12 20:38:45 -0800

    Simon says basic completed

--- src/my_game/my_game_second_main.js
+++ src/my_game/my_game_second_main.js
@@ -4,30 +4,30 @@
  */
 "use strict";  // Operate in Strict mode such that variables must be declared before used!
 
+import Color from "../engine/color.js";
 import engine from "../engine/index.js";
-import Hero from "./objects/hero.js";
-import Minion from "./objects/minion.js";
+import Torch from "./objects/torch.js";
 
 class MyGame extends engine.Scene {
     constructor() {
         super();
-        this.kMinionSprite = "assets/minion_sprite.png";
+        this.kTorch = "assets/torch_sprite.png";
 
         // The camera to view the scene
         this.mCamera = null;
-
-        this.mMsg = null;
-        
-        this.mParticleOptions = ["Default", "Flame", "Dust", "Burst", "Electric", "Snow"];
-        this.mCurrentOption = 0;
-
-        this.mAllObjs = null;
+        this.mTorches = null;
+        this.mPattern = [];
+        this.mInputCounter = 0;
         this.mCollisionInfos = [];
-        this.mHero = null;
+        this.mScore = 0;
 
-        // Draw controls
-        this.mDrawTexture = false;
-        this.mDrawRigidShape = true;
+        this.mDisplayCounter = 0;
+        this.mDisplayTimer = null;
+
+        this.mContinue = true;
+        this.mDisplayPhase = false;
+        this.mInputPhase = false;
+        this.mStartPhase = true;
 
         // Particle Support
         this.mParticles = null;
@@ -35,48 +35,44 @@ class MyGame extends engine.Scene {
         this.mPSCollision = true;
     }
 
-
-
     load() {
-        engine.texture.load(this.kMinionSprite);
+        engine.texture.load(this.kTorch);
     }
 
     unload() {
-        engine.texture.unload(this.kMinionSprite);
+        engine.texture.unload(this.kTorch);
     }
 
     init() {
+        this.mTorches = new engine.GameObjectSet();
         // Step A: set up the cameras
         this.mCamera = new engine.Camera(
             vec2.fromValues(50, 40), // position of the camera
             100,                     // width of camera
             [0, 0, 800, 600]         // viewport (orgX, orgY, width, height)
         );
-        this.mCamera.setBackgroundColor([0.2, 0.2, 0.2, 1]);
+        this.mCamera.setBackgroundColor([0.6, 0.6, 0.6, 1]);
         // sets the background to gray
-        engine.defaultResources.setGlobalAmbientIntensity(3);
-
-        this.mAllObjs = new engine.GameObjectSet();
-
-        this.mHero = new Hero(this.kMinionSprite);
-        //this.mAllObjs.addToSet(this.mHero);
-                
+        let t = new Torch(this.kTorch,75,40,Color.BLUE);
+        let t1 = new Torch(this.kTorch,50,20,Color.RED);
+        let t2 = new Torch(this.kTorch,25,40,Color.GREEN);
+        let t3 = new Torch(this.kTorch,50,60,Color.YELLOW);
+        this.mTorches.addToSet(t);
+        this.mTorches.addToSet(t1);
+        this.mTorches.addToSet(t2);
+        this.mTorches.addToSet(t3);
         // particle systems
         this.mParticles = new engine.ParticleSet();
 
-        // let y = 70;
-        // let x = 10;
-        // for (let i = 1; i <= 5; i++) {
-        //     let m = new Minion(this.kMinionSprite, x, y, ((i % 2) !== 0));
-        //     this.mParticles.addEmitterAt(x, y, 200, _createParticle);
-        //     x += 20;
-        //     this.mAllObjs.addToSet(m);
-        // }
-
-        this.mMsg = new engine.FontRenderable("Status Message");
-        this.mMsg.setColor([0.5, 0.5, 0.5, 1]);
-        this.mMsg.getXform().setPosition(0, 7);
+        this.mMsg = new engine.FontRenderable("Score: 0");
+        this.mMsg.setColor([0, 0, 0, 1]);
+        this.mMsg.getXform().setPosition(10, 7);
         this.mMsg.setTextHeight(3);
+
+        this.mStartMsg = new engine.FontRenderable("Press SPACE to start");
+        this.mStartMsg.setColor([0, 0, 0, 1]);
+        this.mStartMsg.getXform().setPosition(33, 40);
+        this.mStartMsg.setTextHeight(3);
     }
 
     // This is the draw function, make sure to setup proper drawing environment, and more
@@ -87,8 +83,7 @@ class MyGame extends engine.Scene {
 
         this.mCamera.setViewAndCameraMatrix();
 
-        this.mAllObjs.draw(this.mCamera);
-        this.mHero.draw(this.mCamera);
+        this.mMsg.draw(this.mCamera);
 
         this.mParticles.draw(this.mCamera);
         if (this.mPSDrawBounds)
@@ -100,159 +95,101 @@ class MyGame extends engine.Scene {
             this.mCollisionInfos[i].draw(this.mCamera);
         this.mCollisionInfos = [];
         }
-
-        this.mMsg.draw(this.mCamera);
+        this.mTorches.draw(this.mCamera);
+        this.mStartMsg.draw(this.mCamera);
     }
 
     // The Update function, updates the application state. Make sure to _NOT_ draw
     // anything from this function!
     update() {
-        let msg = "";
-
-        this.mAllObjs.update(this.mCamera);
-        this.mHero.update(this.mCamera);
-
-        // if (engine.input.isKeyClicked(engine.input.keys.P)) {
-        //     engine.physics.togglePositionalCorrection();
-        // }
-        if (engine.input.isKeyClicked(engine.input.keys.V)) {
-            engine.physics.toggleHasMotion();
-        }
-        if (engine.input.isKeyClicked(engine.input.keys.F)) {
-            this.randomizeVelocity();
-        }
-        if (engine.input.isKeyClicked(engine.input.keys.R)) {
-            let newRain = this.mParticles.addRain(20,12000);
-            newRain.setColorStart(engine.Color.BLUE);
-            newRain.setColorEnd([.7,.7,.7,.6]);
-            newRain.setWind(20);
-            newRain.setParticleSize(3);
-            newRain.setSizeVariance(0);
-        }
-
-        if (engine.input.isKeyClicked(engine.input.keys.C)) {
-            let x = 20 + Math.random() * 60;
-            let y = 10 + Math.random() * 60;
-            let t = Math.random() > 0.5;
-            let m = new Minion(this.kMinionSprite, x, y, t);
-            if (this.mDrawTexture) // default is false
-                m.toggleDrawRenderable();
-            if (!this.mDrawRigidShape) // default is true
-                m.toggleDrawRigidShape();
-            this.mAllObjs.addToSet(m);
-
-            this.mParticles.addEmitterAt(x, y, 200
-                );
-        }
-        
-        if (engine.input.isKeyClicked(engine.input.keys.Left)) { 
-            if (this.mCurrentOption > 0) {
-                this.mCurrentOption--;
-            } 
-        }
-        if (engine.input.isKeyClicked(engine.input.keys.Right)) {
-            if (this.mCurrentOption < this.mParticleOptions.length - 1) {
-                this.mCurrentOption++;
+        if (this.mStartPhase){
+            if (engine.input.isKeyClicked(engine.input.keys.Space)) { 
+                this.mStartMsg.setText("        Wait...");
+                this.mStartPhase = false;
+                this.mDisplayPhase = true;
+                this.mScore = 0;
+                this.mDisplayTimer = Date.now() + 1000;
             }
         }
-        
+        if (this.mDisplayPhase){
+            if (this.mPattern.length == 0){
+                for (let i = 0; i < 4; i++){
+                    this.mPattern[this.mPattern.length] = parseInt(Math.random()*4);
+                }
+                console.log(this.mPattern);
+            }
+            if (this.mDisplayCounter < this.mPattern.length && Date.now() >= this.mDisplayTimer){
+                let x = this.mTorches.getObjectAt(this.mPattern[this.mDisplayCounter]).getXform().getXPos();
+                let y = this.mTorches.getObjectAt(this.mPattern[this.mDisplayCounter]).getXform().getYPos();
+                let f = this.mParticles.addFlameAt(x,y-2,2,800);
+                f.setParticleSize(8);
+                f.setSizeVariance(4);
+                f.setColorStart(Color.YELLOW);
+                f.setColorEnd(Color.RED);
+                f.setClimb(30);
+                f.setSpread(.6);
+                this.mDisplayTimer = Date.now() + 1500;
+                this.mDisplayCounter++;
+            } else if (this.mDisplayCounter >= this.mPattern.length && Date.now() >= this.mDisplayTimer){
+                this.mDisplayPhase = false;
+                this.mInputPhase = true;
+                this.mDisplayCounter = 0;
+                this.mStartMsg.setText("         Go");
+            }
+        }
+        if (this.mInputPhase){
+            let input = -1;
+            if (engine.input.isKeyClicked(engine.input.keys.Left))
+                input = 2;
+            if (engine.input.isKeyClicked(engine.input.keys.Right))
+                input = 0;
+            if (engine.input.isKeyClicked(engine.input.keys.Up))
+                input = 3;
+            if (engine.input.isKeyClicked(engine.input.keys.Down))
+                input = 1;
+            if (input != -1){
+                let color;
+                if (this.mPattern[this.mInputCounter] == input){
+                    this.mContinue = true;
+                    color = Color.GREEN;
+                    if (this.mScore == this.mInputCounter)
+                        this.mScore++;
+                } else {
+                    this.mContinue = false;
+                    color = Color.RED;
+                }
+                let x = this.mTorches.getObjectAt(input).getXform().getXPos();
+                let y = this.mTorches.getObjectAt(input).getXform().getYPos();
+                let f = this.mParticles.addFlameAt(x,y-2,2,800);
+                f.setParticleSize(8);
+                f.setSizeVariance(4);
+                f.setColorStart(color);
+                f.setColorEnd(Color.BLACK);
+                f.setClimb(30);
+                f.setSpread(.6);
+                this.mInputCounter++;
+            }
+            if (this.mContinue == false){
+                this.mInputPhase = false;
+                this.mStartPhase = true;
+                this.mInputCounter = 0;
+                this.mStartMsg.setText("Press SPACE to start");
+                this.mContinue = true;
+            }
+            if (this.mInputCounter >= this.mPattern.length){
+                for (let i = 0; i < 2; i++){
+                    this.mPattern[this.mPattern.length] = parseInt(Math.random()*4);
+                }
+                this.mInputPhase = false;
+                this.mDisplayPhase = true;
+                this.mStartMsg.setText("        Wait...");
+                this.mDisplayTimer = Date.now() + 1500;
+                this.mInputCounter = 0;
+            }
+        }
+        this.mMsg.setText("Score: " + this.mScore);
         // Particle System
         this.mParticles.update();
-        if (engine.input.isKeyClicked(engine.input.keys.E)) {
-            this.mPSDrawBounds = !this.mPSDrawBounds;
-        }
-            
-        if (engine.input.isButtonClicked(engine.input.eMouseButton.eLeft)) {
-            if (this.mCamera.isMouseInViewport()) {
-                switch(this.mCurrentOption) {
-                    case 0: //Default
-                        let newDefault = this.mParticles.addEmitterAt(this.mCamera.mouseWCX(), this.mCamera.mouseWCY(),100);
-                        newDefault.setColorStart([0,0,1,1]);
-                        newDefault.setColorEnd([0,.7,.3,.6]);
-                        newDefault.setParticleSize(10);
-                        newDefault.setSizeVariance(5);
-                        break;
-                    case 1: //Flame
-                        let newFlame = this.mParticles.addFlameAt(this.mCamera.mouseWCX(), this.mCamera.mouseWCY(),1,4000);
-                        newFlame.setColorStart(engine.Color.RED);
-                        newFlame.setColorEnd([1,.7,.3,.6]);
-                        newFlame.setClimb(20);
-                        newFlame.setSpread(.4);
-                        newFlame.setWind(10);
-                        newFlame.setParticleSize(5);
-                        newFlame.setSizeVariance(1.5);
-                        break;
-                    case 2: //Dust
-                        let newDust = this.mParticles.addDust(1,12000);
-                        newDust.setColorStart([1,1,.6,1]);
-                        newDust.setColorEnd(engine.Color.CLEAR);
-                        newDust.setWind(-20);
-                        newDust.setParticleSize(10);
-                        newDust.setSizeVariance(5);
-                        break;
-                    case 3: //Burst
-                        let newBurst = this.mParticles.addBurstAt(this.mCamera.mouseWCX(), this.mCamera.mouseWCY(),100);
-                        newBurst.setColorStart(engine.Color.GREEN);
-                        newBurst.setColorEnd([1,0,.3,.6]);
-                        newBurst.setParticleSize(10);
-                        newBurst.setSizeVariance(0.5);
-                        break;
-                    case 4: //Electric
-                        let newElectric = this.mParticles.addElectricityAt(this.mCamera.mouseWCX(), this.mCamera.mouseWCY(),10);
-                        newElectric.setColorStart(engine.Color.CYAN);
-                        newElectric.setColorEnd([1,1,1,.6]);
-                        newElectric.setParticleSize(10);
-                        newElectric.setSizeVariance(5);
-                        break;
-                    case 5: //snow
-                        let newSnow = this.mParticles.addSnow(10,12000);
-                        newSnow.setColorStart(engine.Color.WHITE);
-                        newSnow.setColorEnd([.7,.7,.7,.6]);
-                        newSnow.setWind(5);
-                        newSnow.setParticleSize(3);
-                        newSnow.setSizeVariance(2.5);
-                        break;
-                }
-                
-            }
-        }
-        if (engine.input.isKeyClicked(engine.input.keys.Q))
-            this.mPSCollision = !this.mPSCollision;
-        if (this.mPSCollision) {
-            engine.particleSystem.resolveRigidShapeSetCollision(this.mAllObjs, this.mParticles);
-            engine.particleSystem.resolveRigidShapeCollision(this.mHero, this.mParticles);
-            //engine.particleSystem.resolveRigidShapeSetCollision(this.mPlatforms, this.mParticles);
-        }
-
-        this.mHero.keyControl();
-
-        if (engine.input.isKeyClicked(engine.input.keys.H)) {
-            this.randomizeVelocity();
-        }
-
-        //engine.physics.processObjToSet(this.mHero, this.mPlatforms, this.mCollisionInfos);
-        //engine.physics.processSetToSet(this.mAllObjs, this.mPlatforms, this.mCollisionInfos);
-        engine.physics.processSet(this.mAllObjs, this.mCollisionInfos);
-
-        msg += "  Object Count(" + this.mAllObjs.size() + ")" +
-            " Velocity(" + engine.physics.getHasMotion() + ")" +
-            " Particle(" + this.mParticleOptions[this.mCurrentOption] + ")";
-        this.mMsg.setText(msg);
-    }
-}
-
-let kSpeed = 40;
-MyGame.prototype.randomizeVelocity = function()
-{
-    let i = 0;
-    for (i = 0; i<this.mAllObjs.size(); i++) {
-        let obj = this.mAllObjs.getObjectAt(i);
-        let rigidShape = obj.getRigidBody();
-        let x = (Math.random() - 0.5) * kSpeed;
-        let y = (Math.random() - 0.5) * kSpeed;
-        rigidShape.setVelocity(x, y);
-        let c = rigidShape.getCenter();
-        this.mParticles.addEmitterAt(c[0], c[1], 20, this.mPPreset.Basic());
     }
 }
 
